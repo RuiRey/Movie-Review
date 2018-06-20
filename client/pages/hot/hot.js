a7a6958ff8d1d1345a9ee73f1dea5f9e02913385
ruiwang <Rey@xuhongs-MacBook-Pro.local>
2018-06-20 16:56:20 -0700

    hot-movie-list page done

--- client/pages/hot/hot.js
+++ client/pages/hot/hot.js
@@ -1,3 +1,5 @@
+const qcloud = require('../../vendor/wafer2-client-sdk/index')
+const config = require('../../config.js')
 // pages/hot/hot.js
 Page({
 
@@ -5,14 +7,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+   movieList: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getMovieList()
+  },
+
+  getMovieList() {
+    wx.showLoading({
+      title: '电影数据加载中'
+    })
+
+    qcloud.request({
+      url: config.service.movieList,
+      success: result => {
+        wx.hideLoading()
+
+        let data = result.data
+        if (!data.code) {
+          this.setData({
+            movieList: data.data
+          })
+        } else {
+          wx.showToast({
+            title: '电影数据加载失败',
+            icon: 'none'
+          })
+        }
+      },
+      fail: () => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '电影数据加载失败',
+          icon: 'none'
+        })
+      }
+    })
   },
 
   /**
