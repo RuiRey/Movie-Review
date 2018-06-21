b5b86aa09ebd81ed76f9e1a03258e9ab7e423c45
ruiwang <Rey@xuhongs-MacBook-Pro.local>
2018-06-20 17:54:48 -0700

    movieDetail-done except add-review

--- client/pages/movieDetail/movieDetail.js
+++ client/pages/movieDetail/movieDetail.js
@@ -1,18 +1,58 @@
 // pages/movieDetail/movieDetail.js
+const qcloud = require('../../vendor/wafer2-client-sdk/index')
+const config = require('../../config.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    movieDetail:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    this.getMovieDetail(options.id)
+  },
+
+  getMovieDetail(id){
+    wx.showLoading({
+      title: '电影数据加载中'
+    })
+
+    qcloud.request({
+      url: config.service.movieList,
+      success: result => {
+        wx.hideLoading()
+
+        let data = result.data
+        if (!data.code) {
+          let movieList = data.data
+          for (let i = 0; i < movieList.length; i++){
+            if(movieList[i].id == id){
+              this.setData({
+                movieDetail: movieList[i]
+              })
+              break
+            }
+          }
+        } else {
+          wx.showToast({
+            title: '电影数据加载失败',
+            icon: 'none'
+          })
+        }
+      },
+      fail: () => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '电影数据加载失败',
+          icon: 'none'
+        })
+      }
+    })
   },
 
   /**
