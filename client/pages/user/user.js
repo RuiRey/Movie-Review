96d8eb2cb4597cab4ca93674ca44ceb8e23ed084
ruiwang <Rey@xuhongs-MacBook-Pro.local>
2018-07-18 10:50:22 -0700

    userPage myReviewList

--- client/pages/user/user.js
+++ client/pages/user/user.js
@@ -1,4 +1,6 @@
 const app = getApp()
+const qcloud = require('../../vendor/wafer2-client-sdk/index')
+const config = require('../../config.js')
 // pages/user/user.js
 Page({
 
@@ -9,13 +11,79 @@ Page({
     userInfo: null,
     reviewType: ['发布的影评', '收藏的影评'],
     index: 0,
+    movieList: {},
+    myReviewList:[]
   },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getMovieList()
+  },
+
+  getMovieList() {
+    qcloud.request({
+      url: config.service.movieList,
+      success: result => {
+        let data = result.data
+        if (!data.code) {
+          this.setData({
+            movieList: data.data
+          })
+        } 
+      }
+    })
+  },
+
+  getMyReviewList() {
+    wx.showLoading({
+      title: '影评数据加载中'
+    })
+
+    qcloud.request({
+      url: config.service.reviewList,
+      success: result => {
+        wx.hideLoading()
+
+        let data = result.data
+        let userId = this.data.userInfo.openId
+        let myReviewList = []
+        if (!data.code) {
+          let reviews = data.data
+          for (let i = 0; i < reviews.length; i++) {
+            if (reviews[i].user == userId) {
+              myReviewList.push(reviews[i])
+            }
+          }
+          this.setData({
+            myReviewList: myReviewList
+          })
+        } else {
+          wx.showToast({
+            title: '影评数据加载失败',
+            icon: 'none'
+          })
+        }
+      },
+      fail: () => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '影评数据加载失败',
+          icon: 'none'
+        })
+      }
+    })
+  },
+
+
+
 
   bindPickerChange: function (e) {
     //console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       index: e.detail.value
     })
+    this.getMyReviewList()
   },
 
   onTapLogin() {
@@ -28,12 +96,7 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
   
-  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -51,6 +114,7 @@ Page({
         this.setData({
           userInfo
         })
+        this.getMyReviewList()
       }
     })
   },
